90b63278f717d943b61d76c1253415fbe916b1f7
Thiagus Ferreira <thiagus.ferreira@poatek.com>
2021-01-07 21:04:17 -0300

    Improving config for generating the package.

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,14 @@ import typescript from '@rollup/plugin-typescript';
 
 const config = {
     input: 'src/index.tsx',
-    output: {
-        file: 'build/bundle.js',
+    output: [{
+        file: 'dist/farmerconnect-ui.cjs.js',
         format: 'cjs'
     },
+    {
+        file: 'dist/farmerconnect-ui.esm.js',
+        format: 'es'
+    }],
     plugins: [ typescript() ]
 };
 
