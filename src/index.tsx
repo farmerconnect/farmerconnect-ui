1f6a0fd793eb2c1bc5dcfa8227da9029926b1cdd
Thiagus Ferreira <thiagus.ferreira@poatek.com>
2021-03-01 11:12:58 -0300

    Adding navigation to the index to be exported

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,6 @@ import Button from './components/Button';
 import ComboBoxMulti from './components/ComboboxMulti'
 import IconCheck from './components/Icons/Check';
 import IconClose from './components/Icons/Close';
+import NavigationBar from './components/NavigationBar';
 
-export { Breadcrumbs, Button, ComboBoxMulti, IconCheck, IconClose };
+export { Breadcrumbs, Button, ComboBoxMulti, IconCheck, IconClose, NavigationBar };
