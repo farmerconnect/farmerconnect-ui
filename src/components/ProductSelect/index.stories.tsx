3a5dd9cfe6a7f4c5bdeb4e24d91729ffaa8a8bde
wagner <wagner.schwertz@poatek.com>
2021-05-22 14:00:29 -0300

    fixed: removed unused imports

--- src/components/ProductSelect/index.stories.tsx
+++ src/components/ProductSelect/index.stories.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import { useState } from "react";
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from "@storybook/react/types-6-0";
 
-import ProductSelect, { contentType, SelectProps } from ".";
+import ProductSelect, { SelectProps } from ".";
 
 export default {
   title: "ProductSelect",
