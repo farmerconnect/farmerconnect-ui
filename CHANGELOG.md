16c4a634615dd32ef20aa225581b056d943de00e
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2021-06-02 17:56:15 -0300

    Table component documentation improved

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.6.1] - 2021-06-02
+
+- Table component documentation improved
+- Table component exported
+
 ## [1.6.0] - 2021-06-02
 
 - Table component added
--- src/components/Table/index.stories.tsx
+++ src/components/Table/index.stories.tsx
@@ -6,87 +6,98 @@ import Button from '../Button';
 export default {
   title: 'Table',
   component: Table,
+  parameters: {
+    docs: {
+      source: {
+        type: 'code',
+      },
+    },
+  },
 } as Meta;
 
-const Template: Story<any> = (args) => {
+export const Default: Story<any> = () => (
+  <Table
+    columns={[
+      {
+        options: { fitContent: true },
+      },
+      { text: 'Description' },
+      { text: 'Number' },
+      { text: 'ID' },
+      { text: 'Tags' },
+      {
+        options: { fitContent: true },
+      },
+    ]}
+  >
+    {new Array(5).fill('').map((_, i: number) => (
+      <Table.Row key={i}>
+        <Table.Column>
+          <img src="http://via.placeholder.com/80" alt="" />
+        </Table.Column>
+        <Table.Column>Lorem Ipsum</Table.Column>
+        <Table.Column>123456789</Table.Column>
+        <Table.Column>abcde-fghij</Table.Column>
+        <Table.Column>Tag 1</Table.Column>
+        <Table.Column>
+          <Button>Button</Button>
+        </Table.Column>
+      </Table.Row>
+    ))}
+  </Table>
+);
+
+export const Sortable: Story<any> = () => {
   const [sort, setSort] = useState({ key: 'description', order: 'asc' });
 
   const handleSortChange = (key: string, order: string) => setSort({ key, order });
 
-  return <Table {...args} sort={sort} onSortChange={handleSortChange} />;
-};
-
-export const Default = Template.bind({});
-Default.args = {
-  columns: [
-    {
-      options: { fitContent: true },
-    },
-    { text: 'Description' },
-    { text: 'Number' },
-    { text: 'ID' },
-    { text: 'Tags' },
-    {
-      options: { fitContent: true },
-    },
-  ],
-  children: new Array(5).fill('').map((_, i: number) => (
-    <Table.Row key={i}>
-      <Table.Column>
-        <img src="http://via.placeholder.com/80" alt="" />
-      </Table.Column>
-      <Table.Column>Lorem Ipsum</Table.Column>
-      <Table.Column>123456789</Table.Column>
-      <Table.Column>abcde-fghij</Table.Column>
-      <Table.Column>Tag 1</Table.Column>
-      <Table.Column>
-        <Button>Button</Button>
-      </Table.Column>
-    </Table.Row>
-  )),
-};
-
-export const Sortable = Template.bind({});
-Sortable.args = {
-  columns: [
-    {
-      options: { fitContent: true },
-    },
-    {
-      text: 'Description',
-      options: {
-        sortable: { key: 'description' },
-      },
-    },
-    {
-      text: 'Number',
-      options: {
-        sortable: { key: 'number' },
-      },
-    },
-    { text: 'ID' },
-    {
-      text: 'Tags',
-      options: {
-        sortable: { key: 'tags' },
-      },
-    },
-    {
-      options: { fitContent: true },
-    },
-  ],
-  children: new Array(5).fill('').map((_, i: number) => (
-    <Table.Row key={i}>
-      <Table.Column>
-        <img src="http://via.placeholder.com/80" alt="" />
-      </Table.Column>
-      <Table.Column>Lorem Ipsum</Table.Column>
-      <Table.Column>123456789</Table.Column>
-      <Table.Column>abcde-fghij</Table.Column>
-      <Table.Column>Tag 1</Table.Column>
-      <Table.Column>
-        <Button>Button</Button>
-      </Table.Column>
-    </Table.Row>
-  )),
+  return (
+    <Table
+      sort={sort}
+      onSortChange={handleSortChange}
+      columns={[
+        {
+          options: { fitContent: true },
+        },
+        {
+          text: 'Description',
+          options: {
+            sortable: { key: 'description' },
+          },
+        },
+        { text: 'Number' },
+        {
+          text: 'ID',
+          options: {
+            sortable: { key: 'id' },
+          },
+        },
+        {
+          text: 'Tags',
+          options: {
+            sortable: { key: 'tags' },
+          },
+        },
+        {
+          options: { fitContent: true },
+        },
+      ]}
+    >
+      {new Array(5).fill('').map((_, i: number) => (
+        <Table.Row key={i}>
+          <Table.Column>
+            <img src="http://via.placeholder.com/80" alt="" />
+          </Table.Column>
+          <Table.Column>Lorem Ipsum</Table.Column>
+          <Table.Column>123456789</Table.Column>
+          <Table.Column>abcde-fghij</Table.Column>
+          <Table.Column>Tag 1</Table.Column>
+          <Table.Column>
+            <Button>Button</Button>
+          </Table.Column>
+        </Table.Row>
+      ))}
+    </Table>
+  );
 };
