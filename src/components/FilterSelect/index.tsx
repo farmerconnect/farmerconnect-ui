7d10a58576762cd4f666e1437734b589a2024ba1
alexandrefj <alexandre.flores.john@gmail.com>
2021-06-15 14:23:39 -0300

    Features/create a filter select component (#29)
    * added new filter select component
    
    * version incremented
    
    * deleted package json
    
    * addEventListener removed
    
    * replacing to generic type
    
    * corner case to clean selection on input clean up
    
    Co-authored-by: Alexandre John <alexandre.john@poatek.com>

---
+++ src/components/FilterSelect/index.tsx
@@ -0,0 +1,88 @@
+import React, { useState, useEffect, useRef, ReactNode } from 'react';
+import * as S from './styles';
+import { Chevron} from '../SingleSelect/styles';
+
+interface FilterSelectProps<T> {
+  itemList: T[];
+  onSelectItem: (item:T) => void;
+  resolveItemName: (item:T) => string;
+  listItemRender: (item: T) => ReactNode;
+  placeholder:string;
+  noResultsMessage:string;
+}
+
+const FilterSelect = <T extends unknown>({itemList, onSelectItem, listItemRender, resolveItemName, placeholder, noResultsMessage}: FilterSelectProps<T>) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [hasResults, sethasResults] = useState<boolean>(true);
+  const [outputList, setoutputList] = useState<T[]>(itemList);
+  const [inputValue, setinputValue] = useState<string>('');  
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  const handleToggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSelectItem = (item: T) => {
+    if(inputValue === '') {
+      setoutputList(itemList);
+    }
+    onSelectItem(item);  
+    setinputValue(resolveItemName(item));   
+    setIsOpen(false);
+  };
+
+  const handleClickOutside = (e: { target: any }) => {
+    if (!contentRef?.current?.contains(e.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      window.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const onInputChange = (html: React.FormEvent<HTMLInputElement>) => {
+    var searchedText:string = html.currentTarget.value;
+    let filteredList:T[] = itemList;
+    
+    if(searchedText === '') {
+      onSelectItem(null as T);
+    } 
+    if(searchedText !== undefined) {
+      filteredList = itemList.filter((item) => resolveItemName(item).toLowerCase().includes(searchedText.toLowerCase()));
+      sethasResults(filteredList.length !== 0);
+      setIsOpen(true);
+    }
+    setinputValue(searchedText);
+    setoutputList(filteredList);
+  }
+
+  return (
+    <S.Wrapper ref={contentRef}>
+      <S.Heading onClick={() => handleToggleDropdown()} isOpen={isOpen}>
+        <input placeholder={placeholder} onChange={onInputChange} value={inputValue} alt="filter-select-input"></input>
+        <Chevron />
+      </S.Heading>
+      <S.Content isOpen={isOpen}>
+        {
+          hasResults ?
+            <S.ItemList>
+            {outputList.map((item, index) => (
+              <li onClick={() => handleSelectItem(item)} key={`left-content-${index}`}>
+                {listItemRender(item)}
+              </li>
+            ))}
+          </S.ItemList>
+          :
+          <S.EmptyResult>{noResultsMessage}</S.EmptyResult>
+        }
+
+      </S.Content>
+    </S.Wrapper>
+  );
+};
+
+export default FilterSelect;
