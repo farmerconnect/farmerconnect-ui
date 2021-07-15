d50c99756ed333f9c69eb07b4099cdf5c359082f
wagner-schwertz-poatek <84329762+wagner-schwertz-poatek@users.noreply.github.com>
2021-07-15 10:29:20 -0300

    Feat/dropdown select options (#53)
    * added: validation function prop
    added: error feedback
    
    * added: isLoading prop
    
    * added: subitems on DropdownSelect
    
    * fixed: added text prop
    
    * added: versioning and changelog info
    
    * fixed: editableLabel onSave callback
    added: key prop to Radio on DropdownSelect
    
    * fixed: EditableLabel onSave callback and test refactor

--- src/components/DropdownSelect/index.tsx
+++ src/components/DropdownSelect/index.tsx
@@ -1,13 +1,16 @@
-import React, { useState, useRef, ReactNode, useEffect } from 'react';
+import React, { useState, useRef, ReactNode, useEffect, ChangeEvent } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import * as Icon from '../Icons';
 import CustomButton from '../CustomButton';
+import Radio from '../Radio';
 import * as S from './styles';
+import { AnimatePresence } from 'framer-motion';
 
 export type DropdownSelectContentItem = {
   id: string;
-  checked: boolean;
+  checked: string | boolean;
   default: boolean;
+  items?: { id: string; label: ReactNode }[];
 };
 export interface ISelectProps<T extends DropdownSelectContentItem> {
   content?: T[];
@@ -24,6 +27,7 @@ export interface ISelectProps<T extends DropdownSelectContentItem> {
   emptyText?: string;
   disabled?: boolean;
   contentText?: string;
+  noneText?: string;
   isDraggable?: boolean;
   allowEmptySelection?: boolean;
 }
@@ -40,6 +44,7 @@ const DropdownSelect = <T extends DropdownSelectContentItem>({
   confirmButtonText,
   emptyText,
   filterPlaceholderText,
+  noneText = 'none',
   filterSearch = (v, r) => r.id.toLowerCase().includes(v.toLowerCase()),
   contentText,
   isDraggable = false,
@@ -69,6 +74,10 @@ const DropdownSelect = <T extends DropdownSelectContentItem>({
 
   const handleConfirm = () => {
     onConfirmSelection(innerContent);
+  };
+
+  const handleSelectSubItem = (itemId: string | false, subItemId: string | boolean) => {
+    onChange(innerContent.map((item) => (item.id !== itemId ? item : { ...item, checked: subItemId })));
   };
 
   const selectedItems = innerContent.filter((item) => item.checked);
@@ -111,57 +120,57 @@ const DropdownSelect = <T extends DropdownSelectContentItem>({
             )}
           </div>
         </S.FilterInputWrapper>
-        {isDraggable && filterText === '' ? (
-          <DragDropContext onDragEnd={handleOnDragEnd}>
-            <Droppable droppableId="columns">
-              {(provided) => (
-                <S.ListWrapper {...provided.droppableProps} ref={provided.innerRef}>
-                  {!!filteredItems.length &&
-                    filteredItems.map((item, index) => (
-                      <Draggable key={item.id} draggableId={item.id} index={index}>
-                        {(provided, snapshot) => (
-                          <S.ContainerDrag
-                            {...provided.draggableProps}
-                            ref={provided.innerRef}
-                            isDragging={snapshot.isDragging}
-                          >
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId="columns">
+            {(provided) => (
+              <S.ListWrapper {...provided.droppableProps} ref={provided.innerRef}>
+                {!!filteredItems.length &&
+                  filteredItems.map((item, index) => (
+                    <Draggable
+                      key={item.id}
+                      draggableId={item.id}
+                      index={index}
+                      isDragDisabled={!!filterText || !isDraggable}
+                    >
+                      {(provided, snapshot) => (
+                        <S.ContainerDrag
+                          {...provided.draggableProps}
+                          ref={provided.innerRef}
+                          isDragging={snapshot.isDragging}
+                        >
+                          {!filterText && isDraggable && (
                             <div {...provided.dragHandleProps}>
                               <S.HandleIcon />
                             </div>
+                          )}
+                          {!item.items ? (
                             <S.CustomCheckbox
-                              checked={innerContent.find((i) => i.id === item.id)!.checked}
+                              checked={!!innerContent.find((i) => i.id === item.id)!.checked}
                               onChange={() => handleToggleSelectItem(item)}
                               disabled={(!item.checked && selectedItems.length === limit) || item.default}
                             >
                               {itemRenderer(item)}
                             </S.CustomCheckbox>
-                          </S.ContainerDrag>
-                        )}
-                      </Draggable>
-                    ))}
-                  {provided.placeholder}
-                  {filteredItems.length === 0 && filterText && <S.EmptyMessage>{emptyText}</S.EmptyMessage>}
-                </S.ListWrapper>
-              )}
-            </Droppable>
-          </DragDropContext>
-        ) : (
-          <S.ListWrapper>
-            {!!filteredItems.length &&
-              filteredItems.map((item, index) => (
-                <S.ContainerDrag isDragging={false} key={index}>
-                  <S.CustomCheckbox
-                    checked={innerContent.find((i) => i.id === item.id)!.checked}
-                    onChange={() => handleToggleSelectItem(item)}
-                    disabled={(!item.checked && selectedItems.length === limit) || item.default}
-                  >
-                    {itemRenderer(item)}
-                  </S.CustomCheckbox>
-                </S.ContainerDrag>
-              ))}
-            {filteredItems.length === 0 && filterText && <S.EmptyMessage>{emptyText}</S.EmptyMessage>}
-          </S.ListWrapper>
-        )}
+                          ) : (
+                            <AccordionList
+                              id={item.id}
+                              items={item.items}
+                              selected={item.checked}
+                              label={itemRenderer(item)}
+                              onSelect={(subItemId) => handleSelectSubItem(item.id, subItemId)}
+                              noneText={noneText}
+                            />
+                          )}
+                        </S.ContainerDrag>
+                      )}
+                    </Draggable>
+                  ))}
+                {provided.placeholder}
+                {filteredItems.length === 0 && filterText && <S.EmptyMessage>{emptyText}</S.EmptyMessage>}
+              </S.ListWrapper>
+            )}
+          </Droppable>
+        </DragDropContext>
         <S.ButtonContainer>
           <S.CustomizedButton variant="link" onClick={() => handleCancel()}>
             {clearButtonText}
@@ -175,6 +184,60 @@ const DropdownSelect = <T extends DropdownSelectContentItem>({
         </S.ButtonContainer>
       </S.Content>
     </S.Wrapper>
+  );
+};
+
+type AccordionListProps<T extends { id: string; label: ReactNode }> = {
+  id: string;
+  label?: ReactNode;
+  items?: T[];
+  onSelect?: (t: string | boolean) => void;
+  selected?: string | boolean;
+  noneText?: string;
+};
+const AccordionList = <T extends { id: string; label: ReactNode }>({
+  label,
+  id,
+  items = [] as T[],
+  selected = false,
+  onSelect = (_) => {},
+  noneText = 'none',
+}: AccordionListProps<T>) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleFormChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onSelect(e.target.value || false);
+  };
+
+  const handleToggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <S.CollapsableItem onClick={handleToggleOpen} className={isOpen ? 'open' : ''}>
+        <S.ChevronRight /> {label}
+      </S.CollapsableItem>
+      <AnimatePresence>
+        {isOpen && (
+          <S.AccordionContainer
+            animate={{ height: 'auto' }}
+            initial={{ height: 0 }}
+            exit={{ height: 0 }}
+            transition={{ duration: 0.2 }}
+          >
+            <Radio name={id} value="" checked={!selected} onChange={handleFormChange}>
+              <span>{noneText}</span>
+            </Radio>
+            {items.map((item) => (
+              <Radio name={id} value={item.id} checked={selected === item.id} onChange={handleFormChange} key={item.id}>
+                <span>{item.label}</span>
+              </Radio>
+            ))}
+          </S.AccordionContainer>
+        )}
+      </AnimatePresence>
+    </div>
   );
 };
 
