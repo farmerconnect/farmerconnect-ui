3d596de4814d5d7abc8943fd35ba887ba3f84988
Tiago Almeida <tiago.almeida@poatek.com>
2021-07-22 16:58:14 -0300

    Added new Breadcrumbs

--- src/components/Breadcrumbs/styles.ts
+++ src/components/Breadcrumbs/styles.ts
@@ -4,8 +4,4 @@ export const Container = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  justify-content: center;
-  padding-bottom: 2rem;
-  padding-top: 1rem;
-  width: 100%;
 `;
