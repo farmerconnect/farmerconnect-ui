ab1a0522e1c86b327e2fd7d190ce5c7130935d04
jotape-poatek <73607071+jotape-poatek@users.noreply.github.com>
2021-08-20 15:30:00 -0300

    Included labelSwitch (#76)
    * fix test
    
    * fix default labels

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.30.0] - 2021-08-20
+
+- Added LabelSwitch component
+
 ## [1.29.1] - 2021-08-19
 
 - Added resolveItemRender prop in FilterSelect
---
+++ src/components/LabelSwitch/index.stories.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import LabelSwitch from '.';
+
+export default {
+  title: 'Label Switch',
+  component: LabelSwitch,
+  argTypes: {},
+  args: {
+    labels: ['Label 1', 'Label 2'],
+  },
+} as Meta;
+
+const Template: Story = (args) => {
+  const [label, setLabel] = useState(0);
+
+  return (
+    <div style={{ display: 'flex' }}>
+      <LabelSwitch {...args} onChange={setLabel} currentLabel={label} />
+    </div>
+  );
+};
+
+export const Default = Template.bind({});
+
+export const Disabled = Template.bind({});
+Disabled.args = { labels: ['Label 1', 'Label 2'], disabledLabels: [1] };
