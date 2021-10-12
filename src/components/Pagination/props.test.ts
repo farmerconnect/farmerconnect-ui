c7066afb1a808968f3b2c5d03680d1f7bdf62fa5
Damien Frew <damien.frew@farmerconnect.com>
2021-10-12 13:32:25 +0200

    Split pagination test files.

---
+++ src/components/Pagination/props.test.ts
@@ -0,0 +1,155 @@
+import { IPaginationProps, isIPaginationProps } from './interfaces';
+const consoleErrorSpy = jest.spyOn(global.console, 'error');
+
+describe('isIPaginationProps', () => {
+    const mockProps: IPaginationProps = {
+      pagination: {
+        pageSize: 10,
+        currentPageIndex: 2,
+        totalItemCount: 100,
+      },
+      onPaginate: () => {},
+    };
+  
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+  
+    it('should validate valid values', () => {
+      expect(isIPaginationProps(mockProps)).toBe(true);
+    });
+  
+    it('should validate edge cases', () => {
+      expect(
+        isIPaginationProps({
+          ...mockProps,
+          pagination: {
+            pageSize: 10,
+            currentPageIndex: 0,
+            totalItemCount: 0,
+          },
+        })
+      ).toBe(true);
+    });
+  
+    describe('pageSize validation', () => {
+      it('should catch invalid pageSize', () => {
+        expect(
+          isIPaginationProps({
+            ...mockProps,
+            pagination: {
+              ...mockProps.pagination,
+              pageSize: -1,
+            },
+          })
+        ).toBe(false);
+      });
+  
+      it('should console.error when pageSize fails', () => {
+        isIPaginationProps({
+          ...mockProps,
+          pagination: {
+            ...mockProps.pagination,
+            pageSize: -1,
+          },
+        });
+        expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
+      });
+    });
+  
+    describe('currentPageIndex validation', () => {
+      it('should catch negative values', () => {
+        expect(
+          isIPaginationProps({
+            ...mockProps,
+            pagination: {
+              ...mockProps.pagination,
+              currentPageIndex: -1,
+            },
+          })
+        ).toBe(false);
+      });
+  
+      it('should console.error when invalid', () => {
+        isIPaginationProps({
+          ...mockProps,
+          pagination: {
+            ...mockProps.pagination,
+            currentPageIndex: -1,
+          },
+        });
+        expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
+      });
+  
+      it('if hasSelect is true, should catch values that are not in pageSizeArray', () => {
+        expect(
+          isIPaginationProps({
+            ...mockProps,
+            hasSelect: true,
+            pageSizeArray: [],
+            pagination: {
+              pageSize: 10,
+              currentPageIndex: 2,
+              totalItemCount: 100,
+            },
+          })
+        ).toBe(false);
+      });
+  
+      it('if hasSelect is false, should ignore pageSizeArray', () => {
+        expect(
+          isIPaginationProps({
+            ...mockProps,
+            hasSelect: false,
+            pageSizeArray: [],
+            pagination: {
+              pageSize: 10,
+              currentPageIndex: 2,
+              totalItemCount: 100,
+            },
+          })
+        ).toBe(true);
+      });
+  
+      it('should catch values that exceeds the number of pages', () => {
+        expect(
+          isIPaginationProps({
+            ...mockProps,
+            pagination: {
+              pageSize: 10,
+              currentPageIndex: 5,
+              totalItemCount: 0,
+            },
+          })
+        ).toBe(false);
+      });
+    });
+  
+    describe('totalItemCount', () => {
+      it('should catch negative values', () => {
+        expect(
+          isIPaginationProps({
+            ...mockProps,
+            pagination: {
+              ...mockProps.pagination,
+              currentPageIndex: 0,
+              totalItemCount: -1,
+            },
+          })
+        ).toBe(false);
+      });
+  
+      it('should console.error when invalid', () => {
+        isIPaginationProps({
+          ...mockProps,
+          pagination: {
+            ...mockProps.pagination,
+            currentPageIndex: 0,
+            totalItemCount: -1,
+          },
+        });
+        expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
+      });
+    });
+  });
+  
--- src/components/Pagination/utils.test.ts
+++ src/components/Pagination/utils.test.ts
@@ -1,4 +1,3 @@
-import { IPaginationProps, isIPaginationProps } from './interfaces';
 import {
   generatePageNumberArray,
   updatePageNumber,
@@ -6,8 +5,6 @@ import {
   getFirstItemOnCurrentPage,
   getLastItemOnCurrentPage,
 } from './utils';
-
-const consoleErrorSpy = jest.spyOn(global.console, 'error');
 
 describe('Pagination utils', () => {
   describe('getLastPageIndex', () => {
@@ -260,7 +257,7 @@ describe('Pagination utils', () => {
         ],
       },
       {
-        name: 'should low page number case',
+        name: 'should handle setting the page number to a lower value',
         minPages: 5,
         pagination: { currentPageIndex: 2, pageSize: 10, totalItemCount: 45 },
         expected: [
@@ -277,160 +274,4 @@ describe('Pagination utils', () => {
       });
     });
   });
-});
-
-describe('isIPaginationProps', () => {
-  const mockProps: IPaginationProps = {
-    pagination: {
-      pageSize: 10,
-      currentPageIndex: 2,
-      totalItemCount: 100,
-    },
-    onPaginate: () => {},
-  };
-
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
-  it('should validate valid values', () => {
-    expect(isIPaginationProps(mockProps)).toBe(true);
-  });
-
-  it('should validate edge cases', () => {
-    expect(
-      isIPaginationProps({
-        ...mockProps,
-        pagination: {
-          pageSize: 10,
-          currentPageIndex: 0,
-          totalItemCount: 0,
-        },
-      })
-    ).toBe(true);
-  });
-
-  describe('pageSize validation', () => {
-    it('should catch invalid pageSize', () => {
-      expect(
-        isIPaginationProps({
-          ...mockProps,
-          pagination: {
-            ...mockProps.pagination,
-            pageSize: -1,
-          },
-        })
-      ).toBe(false);
-    });
-
-    it('should console.error when pageSize fails', () => {
-      isIPaginationProps({
-        ...mockProps,
-        pagination: {
-          ...mockProps.pagination,
-          pageSize: -1,
-        },
-      });
-      expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
-    });
-  });
-
-  describe('currentPageIndex validation', () => {
-    it('should catch negative values', () => {
-      expect(
-        isIPaginationProps({
-          ...mockProps,
-          pagination: {
-            ...mockProps.pagination,
-            currentPageIndex: -1,
-          },
-        })
-      ).toBe(false);
-    });
-
-    it('should console.error when invalid', () => {
-      isIPaginationProps({
-        ...mockProps,
-        pagination: {
-          ...mockProps.pagination,
-          currentPageIndex: -1,
-        },
-      });
-      expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
-    });
-
-    it('if hasSelect is true, should catch values that are not in pageSizeArray', () => {
-      expect(
-        isIPaginationProps({
-          ...mockProps,
-          hasSelect: true,
-          pageSizeArray: [],
-          pagination: {
-            pageSize: 10,
-            currentPageIndex: 2,
-            totalItemCount: 100,
-          },
-        })
-      ).toBe(false);
-    });
-
-    it('if hasSelect is false, should ignore pageSizeArray', () => {
-      expect(
-        isIPaginationProps({
-          ...mockProps,
-          hasSelect: false,
-          pageSizeArray: [],
-          pagination: {
-            pageSize: 10,
-            currentPageIndex: 2,
-            totalItemCount: 100,
-          },
-        })
-      ).toBe(true);
-    });
-
-    it('should catch values that exceeds the number of pages', () => {
-      expect(
-        isIPaginationProps({
-          ...mockProps,
-          pagination: {
-            pageSize: 10,
-            currentPageIndex: 5,
-            totalItemCount: 0,
-          },
-        })
-      ).toBe(false);
-    });
-  });
-
-  describe('totalItemCount', () => {
-    it('should catch negative values', () => {
-      expect(
-        isIPaginationProps({
-          ...mockProps,
-          pagination: {
-            ...mockProps.pagination,
-            currentPageIndex: 0,
-            totalItemCount: -1,
-          },
-        })
-      ).toBe(false);
-    });
-
-    it('should console.error when invalid', () => {
-      isIPaginationProps({
-        ...mockProps,
-        pagination: {
-          ...mockProps.pagination,
-          currentPageIndex: 0,
-          totalItemCount: -1,
-        },
-      });
-      expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
-    });
-  });
-});
-
-describe('Pagination Component', () => {
-  // TODO add things
 });
