b256ada6809bfa6c230a081da4493a159d4d376a
Damien Frew <damien.frew@farmerconnect.com>
2021-10-13 20:39:32 +0200

    chore: wait for rendering in DatePicker tests.

--- src/components/DatePicker/index.test.tsx
+++ src/components/DatePicker/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import DatePicker from '.';
 
 describe('DatePicker component', () => {
@@ -7,28 +7,30 @@ describe('DatePicker component', () => {
     expect(container.getByPlaceholderText(/select date.../i)).toBeInTheDocument();
   });
 
-  it('opens on click', () => {
+  it('opens on click', async () => {
     const container = render(<DatePicker start={new Date()} />);
     fireEvent.click(container.getByPlaceholderText(/select date.../i));
-    expect(container.getByText(/day/i)).toBeInTheDocument();
+    await waitFor(() => expect(container.getByText(/day/i)).toBeInTheDocument());
   });
 
-  it('toggles properly between single date and range date selection', () => {
+  it('toggles properly between single date and range date selection', async () => {
     const container = render(<DatePicker start={new Date()} />);
     fireEvent.click(container.getByPlaceholderText(/select date.../i));
-    expect(container.getByText(/clear dates/i)).toBeInTheDocument();
+    await waitFor(() => expect(container.getByText(/clear dates/i)).toBeInTheDocument());
     fireEvent.click(container.getByText(/day/i));
-    expect(container.queryByText(/clear dates/i)).not.toBeInTheDocument();
+    await waitFor(() => expect(container.queryByText(/clear dates/i)).not.toBeInTheDocument());
   });
 
-  it('calls onChange with the correct arguments for single date selection', () => {
+  it('calls onChange with the correct arguments for single date selection', async () => {
     const onChange = jest.fn();
     const targetDate = new Date(2021, 0, 2);
     const container = render(<DatePicker start={new Date(2021, 0, 1)} selectsRange={false} onChange={onChange} />);
     fireEvent.click(container.getByPlaceholderText(/select date.../i));
-    expect(container.getByText(/day/i)).toBeInTheDocument();
+    await waitFor(() => expect(container.getByText(/day/i)).toBeInTheDocument());
     fireEvent.click(container.getByRole('button', { name: 'Choose Saturday, January 2nd, 2021' }));
-    expect(container.queryByText(/day/i)).not.toBeInTheDocument();
-    expect(onChange).toHaveBeenCalledWith(targetDate, targetDate);
+    await waitFor(() => {
+      expect(container.queryByText(/day/i)).not.toBeInTheDocument();
+      expect(onChange).toHaveBeenCalledWith(targetDate, targetDate);
+    });
   });
 });
