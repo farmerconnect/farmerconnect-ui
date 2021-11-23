c0ab087f60d40cc80443c83aa1b502948b38e1f3
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2021-11-23 06:04:36 -0300

    Custom icon removed from stories

--- src/components/CustomButton/index.stories.tsx
+++ src/components/CustomButton/index.stories.tsx
@@ -4,6 +4,7 @@ import { Story, Meta } from '@storybook/react/types-6-0';
 
 import Button from '.';
 import { ButtonProps } from './styles';
+import * as Icon from '../Icons';
 
 export default {
   title: 'Custom Button',
@@ -18,29 +19,6 @@ export default {
   },
 } as Meta;
 
-const CloseIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg width="13" height="14" viewBox="0 0 13 14" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <rect
-      x="2.28223"
-      y="1.00012"
-      width="15.1576"
-      height="2"
-      rx="1"
-      transform="rotate(45 2.28223 1.00012)"
-      fill="param(color) #FFF"
-    />
-    <rect
-      x="13"
-      y="2.28198"
-      width="15.1576"
-      height="2"
-      rx="1"
-      transform="rotate(135 13 2.28198)"
-      fill="param(color) #FFF"
-    />
-  </svg>
-);
-
 const Template: Story<ButtonProps> = (args) => (
   <div
     style={{
@@ -50,16 +28,16 @@ const Template: Story<ButtonProps> = (args) => (
   >
     <Button {...args} />
     <Button {...args}>
-      <CloseIcon /> Custom Button
+      <Icon.Close fill="currentColor" /> Custom Button
     </Button>
     <Button {...args}>
       Custom Button
-      <CloseIcon />
+      <Icon.Close fill="currentColor" />
     </Button>
     <Button {...args} disabled />
     <Button {...args} disabled>
       Disabled Icon Button
-      <CloseIcon />
+      <Icon.Close fill="currentColor" />
     </Button>
   </div>
 );
