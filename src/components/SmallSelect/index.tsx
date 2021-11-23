cb8aae907030f789827387f3727c04ddf7e15d02
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2021-11-23 07:17:03 -0300

    Arrow icon fixed on SmallSelect

--- src/components/SmallSelect/index.tsx
+++ src/components/SmallSelect/index.tsx
@@ -2,20 +2,21 @@ import * as S from './styles';
 import { components, Props, OptionProps } from 'react-select';
 import { CheckboxUnchecked, CheckboxChecked } from '../Icons/Checkbox';
 import { HelperText } from '../Input/styles';
+import { Arrow } from '../Icons';
 
 export type SmallSelectProps = {
   error?: string | boolean;
   helperText?: string;
 } & Props;
 
-const CustomOption = ({ children, ...props }: OptionProps<{}, boolean>) => {
-  return (
-    <components.Option {...props}>
-      {props.isMulti ? props.isSelected ? <CheckboxChecked className="checked" /> : <CheckboxUnchecked /> : null}
-      {children}
-    </components.Option>
-  );
-};
+const CustomOption = ({ children, ...props }: OptionProps<{}, boolean>) => (
+  <components.Option {...props}>
+    {props.isMulti ? props.isSelected ? <CheckboxChecked className="checked" /> : <CheckboxUnchecked /> : null}
+    {children}
+  </components.Option>
+);
+
+const DropdownIcon = () => <Arrow size="small" />;
 
 const SmallSelect = (props: SmallSelectProps) => (
   <S.SelectWrapper>
@@ -34,16 +35,6 @@ const SmallSelect = (props: SmallSelectProps) => (
     {props.error && typeof props.error === 'string' && <HelperText error={true}>{props.error}</HelperText>}
     {props.helperText && !props.error && <HelperText>{props.helperText}</HelperText>}
   </S.SelectWrapper>
-);
-
-const DropdownIcon = () => (
-  <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M9.73692 0.266515C9.38765 -0.0888383 8.82345 -0.0888383 8.47419 0.266515L4.99943 3.80182L1.52468 0.266515C1.17542 -0.0888383 0.611217 -0.0888383 0.26195 0.266515C-0.0873156 0.621868 -0.0873156 1.1959 0.26195 1.55125L4.37255 5.73349C4.72181 6.08884 5.28601 6.08884 5.63528 5.73349L9.74588 1.55125C10.0862 1.20501 10.0862 0.621868 9.73692 0.266515Z"
-      fill="#141414"
-      fillOpacity="0.7"
-    />
-  </svg>
 );
 
 export default SmallSelect;
