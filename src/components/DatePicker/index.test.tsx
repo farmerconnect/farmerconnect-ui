1a898e35102f5b28ae153625e21c3da3ba222c0a
Wagner Schwertz <wagner.schwertz@poatek.com>
2021-11-25 16:07:22 -0300

    merged main

--- src/components/DatePicker/index.test.tsx
+++ src/components/DatePicker/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, fireEvent, waitFor } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import DatePicker from '.';
 
 describe('DatePicker component', () => {
@@ -10,15 +10,7 @@ describe('DatePicker component', () => {
   it('opens on click', () => {
     const container = render(<DatePicker start={new Date()} />);
     fireEvent.click(container.getByPlaceholderText(/select date.../i));
-    expect(container.getByText(/day/i)).toBeInTheDocument();
-  });
-
-  it('toggles properly between single date and range date selection', () => {
-    const container = render(<DatePicker start={new Date()} />);
-    fireEvent.click(container.getByPlaceholderText(/select date.../i));
-    expect(container.getByText(/clear dates/i)).toBeInTheDocument();
-    fireEvent.click(container.getByText(/day/i));
-    expect(container.queryByText(/clear dates/i)).not.toBeInTheDocument();
+    expect(container.getByLabelText(/date range/i)).toBeChecked();
   });
 
   it('calls onChange with the correct arguments for single date selection', () => {
@@ -26,9 +18,23 @@ describe('DatePicker component', () => {
     const targetDate = new Date(2021, 0, 2);
     const container = render(<DatePicker start={new Date(2021, 0, 1)} selectsRange={false} onChange={onChange} />);
     fireEvent.click(container.getByPlaceholderText(/select date.../i));
-    expect(container.getByText(/day/i)).toBeInTheDocument();
+    expect(container.getByLabelText(/date range/i)).toBeInTheDocument();
+    expect(container.getByLabelText(/date range/i)).not.toBeChecked();
     fireEvent.click(container.getByRole('button', { name: 'Choose Saturday, January 2nd, 2021' }));
-    expect(container.queryByText(/day/i)).not.toBeInTheDocument();
     expect(onChange).toHaveBeenCalledWith(targetDate, targetDate);
+  });
+
+  it('calls onChange with the correct arguments for date range selection', () => {
+    const onChange = jest.fn();
+    let startDate: Date | null = new Date(2021, 0, 1);
+    let endDate: Date | null = new Date(2021, 0, 2);
+    const container = render(<DatePicker start={startDate} end={endDate} selectsRange={true} onChange={onChange} />);
+    fireEvent.click(container.getByPlaceholderText(/select date.../i));
+    expect(container.getByLabelText(/date range/i)).toBeChecked();
+    fireEvent.click(container.getByRole('button', { name: 'Choose Sunday, January 3rd, 2021' }));
+    expect(onChange).toHaveBeenCalledWith(new Date(2021, 0, 3), null);
+    expect(container.getByLabelText(/date range/i)).toBeInTheDocument();
+    fireEvent.click(container.getByRole('button', { name: 'Choose Monday, January 4th, 2021' }));
+    expect(onChange).toHaveBeenCalledWith(new Date(2021, 0, 4), null);
   });
 });
--- src/components/Select/styles.tsx
+++ src/components/Select/styles.tsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 import { customScrollbar } from '../../mixins';
-import SmallSelect from '../SmallSelect';
+import { Select as SmallSelect } from '../SmallSelect/styles';
 import { farmerConnectTheme } from '../Theme';
+export { SelectWrapper, FooterContainer } from '../SmallSelect/styles';
+export { HelperText } from '../Input/styles';
 
 export const Select = styled(SmallSelect)`
   .select__control {
@@ -9,7 +11,7 @@ export const Select = styled(SmallSelect)`
     border-color: transparent;
     min-height: 2.5rem;
     max-height: 2.5rem;
-    box-shadow: ${({ error }) => (error ? `0.125rem 0.125rem 0 farmerConnectTheme.colors.fc_red` : 'none')};
+    box-shadow: ${({ error }) => (error ? `0.125rem 0.125rem 0 ${farmerConnectTheme.colors.fc_red}` : 'none')};
     transition: box-shadow 0.1s ease-out;
 
     &--is-focused,
@@ -56,7 +58,17 @@ export const Select = styled(SmallSelect)`
 
   .select__indicators > svg {
     color: ${farmerConnectTheme.colors.fc_black_70};
-    right: 0.75rem;
-    top: 0.5rem;
+    right: 1rem;
+    top: calc(50% - 0.25rem);
+  }
+  .select__single-value-value {
+    font-weight: 500;
+  }
+
+  .select__option {
+    &:hover,
+    &--is-focused {
+      background-color: ${farmerConnectTheme.colors.fc_black_10};
+    }
   }
 `;
