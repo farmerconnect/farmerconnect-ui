2432cb3cc308b79d93b04a0b55ecce8cd4b68fa4
Damien Frew <damien.frew@farmerconnect.com>
2021-11-30 19:28:45 +0100

    Remove icons from link and cancel variants

--- src/components/CustomButton/index.tsx
+++ src/components/CustomButton/index.tsx
@@ -4,29 +4,30 @@ import * as S from './styles';
 const RawText: React.FC<{ text: string; iconOnly?: boolean }> = ({ text, iconOnly }) =>
   !iconOnly ? <span className="fc-button__raw-text">{text}</span> : null;
 
-const CustomButton: React.FC<S.ButtonProps> = ({ children, ...props }) => {
-  const { variant, iconOnly } = props;
+const CustomButton: React.FC<S.ButtonProps> = ({ children, variant, ...props }) => {
+  const { iconOnly } = props;
 
-  const childrenClone = (() => {
-    if (typeof children === 'string') return <RawText text={children} iconOnly={iconOnly} />;
+  const safeChildren = React.Children.toArray(children);
 
-    if (Array.isArray(children)) {
-      return children.map((child, i) =>
-        typeof child === 'string' ? (
-          <RawText key={`${typeof child}-${i}`} text={child} iconOnly={iconOnly} />
-        ) : (
-          <React.Fragment key={`${typeof child}-${i}`}>{child}</React.Fragment>
-        )
-      );
-    }
+  if (variant === 'cancel') {
+    // eslint-disable-next-line no-console
+    console.warn('CustomButton variant=cancel is deprecated, please use variant=link instead.');
+    variant = 'link';
+  }
 
-    return children;
-  })();
-
-  // eslint-disable-next-line no-console
-  if (variant === 'cancel') console.warn('CustomButton variant=cancel is deprecated, please use variant=link instead.');
-
-  return <S.Button {...props}>{childrenClone}</S.Button>;
+  return (
+    <S.Button {...props} variant={variant}>
+      {safeChildren.map((child, i) => {
+        if (typeof child === 'string') return <RawText key={`${typeof child}-${i}`} text={child} iconOnly={iconOnly} />;
+        if (variant === 'link') {
+          // eslint-disable-next-line no-console
+          console.warn('CustomButton variant=link does not support children nodes that are not text.');
+          return null;
+        }
+        return <React.Fragment key={`${typeof child}-${i}`}>{child}</React.Fragment>;
+      })}
+    </S.Button>
+  );
 };
 
 export default CustomButton;
