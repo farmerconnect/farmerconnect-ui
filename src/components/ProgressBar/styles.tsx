269642bf6d9358f0cc00c436b089be28152a4d90
Arthur Foscarini <84153540+arthurfoscarini-poatek@users.noreply.github.com>
2021-12-10 16:17:03 -0300

    Update ProgressBar component to handle color of the track and bar height as props (#153)
    * Update ProgressBar component to handle color of the track and bar height as props
    
    * Upgraded version to 3.0.0

--- src/components/ProgressBar/styles.tsx
+++ src/components/ProgressBar/styles.tsx
@@ -1,16 +1,21 @@
 import styled from 'styled-components';
-import { IProgressBar } from './interfaces';
+import { farmerConnectTheme } from '../Theme';
+import { IProgressBar, IProgressBarContainer } from './interfaces';
 
-export const Container = styled.div`
-  height: 4px;
+const defaultTrackHeight = 4;
+const defaultTrackColor = farmerConnectTheme.colors.fc_black_5;
+const defaultBackgroundColor = farmerConnectTheme.colors.fc_green;
+
+export const Container = styled.div<IProgressBarContainer>`
+  height: ${({ height }) => (!!height ? height : defaultTrackHeight)}px;
   width: 100%;
-  background-color: #F3F3F3;
+  background-color: ${({ trackColor }) => (!!trackColor ? farmerConnectTheme.colors[trackColor] : defaultTrackColor)};
   border-radius: 7px;
 `;
 
 export const Filler = styled.div<IProgressBar>`
   height: 100%;
   width: ${props => props.completed}%;
-  background-color: ${props => props.backgroundColor};
+  background-color: ${({ backgroundColor }) => (!!backgroundColor ? farmerConnectTheme.colors[backgroundColor] : defaultBackgroundColor)};;
   border-radius: inherit;
 `;
